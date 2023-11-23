aff69d5c2def0db93454b8d397165698208a063b
SalazarEmiliano <salazaremiliano.dev@gmail.com>
2023-11-23 01:09:11 +0100

    Default icon added

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,9 @@
 // Map.js
 
 import React, { useEffect, useRef } from 'react';
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
+import Leaflet from 'leaflet';
 import '../styles/map.css';
 
 const Map = ({ center, restaurants, onMarkerClick, highlightedRestaurantId }) => {
@@ -17,7 +17,6 @@ const Map = ({ center, restaurants, onMarkerClick, highlightedRestaurantId }) =>
         restaurant.coordinates.longitude !== null
     );
 
-    // Update the center based on the first valid restaurant
     if (validRestaurants.length > 0 && mapRef.current) {
       const firstRestaurant = validRestaurants[0];
       mapRef.current.setView(
@@ -31,24 +30,6 @@ const Map = ({ center, restaurants, onMarkerClick, highlightedRestaurantId }) =>
     onMarkerClick(restaurant);
   };
 
-  const createCustomIcon = (restaurant) => {
-    const { image_url } = restaurant;
-
-    // Use the default Leaflet icon if no image URL is provided
-    if (!image_url) {
-      return L.icon();
-    }
-
-    // Use a custom icon with a fallback image
-    return new L.Icon({
-      className: "map-icon",
-      iconUrl: image_url,
-      iconSize: [32, 32],
-      iconAnchor: [16, 32],
-      popupAnchor: [0, -32],
-    });
-  };
-
   return (
     <div style={{ maxWidth: '800px', margin: '0 auto', overflow: 'hidden' }}>
       <MapContainer ref={mapRef} center={center} zoom={15} style={{ height: '100vh', width: '100%' }}>
@@ -56,35 +37,44 @@ const Map = ({ center, restaurants, onMarkerClick, highlightedRestaurantId }) =>
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        {restaurants.map(
-          (restaurant) =>
+        {restaurants.map((restaurant) => {
+          if (
             restaurant.coordinates &&
             restaurant.coordinates.latitude !== null &&
-            restaurant.coordinates.longitude !== null ? (
+            restaurant.coordinates.longitude !== null
+          ) {
+            const isHighlighted = highlightedRestaurantId === restaurant.id;
+
+            const icon = Leaflet.icon({
+              className: isHighlighted ? 'map-icon marker-selected' : 'map-icon',
+              iconUrl: restaurant.image_url || '../../default_icon.png',
+              iconSize: isHighlighted ? [50, 50] : [32, 32],
+              iconAnchor: isHighlighted ? [20, 40] : [16, 32],
+              popupAnchor: isHighlighted ? [0, -40] : [0, -32],
+            });
+
+            return (
               <Marker
                 key={restaurant.id}
                 position={[
                   restaurant.coordinates.latitude,
                   restaurant.coordinates.longitude,
                 ]}
-                icon={
-                  highlightedRestaurantId === restaurant.id
-                    ? L.icon({
-                        className: "map-icon marker-selected",
-                        iconUrl: restaurant.image_url,
-                        iconSize: [50, 50],
-                        iconAnchor: [20, 40],
-                        popupAnchor: [0, -40],
-                      })
-                    : createCustomIcon(restaurant)
-                }
+                icon={icon}
                 eventHandlers={{
                   click: () => handleMarkerClick(restaurant),
                 }}
               >
+                {isHighlighted && (
+                  <Popup>
+                    <div>Highlighted Restaurant</div>
+                  </Popup>
+                )}
               </Marker>
-            ) : null
-        )}
+            );
+          }
+          return null;
+        })}
       </MapContainer>
     </div>
   );
